6601e1d478d9c852663547566e403724e88eed42
m7mood <mahmoud.azamtta19@gmail.com>
2024-12-21 11:41:22 +0200

    Fetch courses from api

--- sections/Products/Products.tsx
+++ sections/Products/Products.tsx
@@ -1,125 +1,40 @@
 "use client";
 
 import { Box } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProductCard from "@/components/ProductCard/ProductCard";
-import CardIcon from "@/icons/CardIcon";
-import PythonIcon from "@/icons/PythonIcon";
-import JavaIcon from "@/icons/JavaIcon";
-import HTMLIcon from "@/icons/HTMLIcon";
-import Diamond from "@/icons/Diamond";
-import Bootstrap from "@/icons/Bootstrap";
-import CSharpIcon from "@/icons/CSharpIcon";
-import PHPIcon from "@/icons/PHPIcon";
 import CoursesSlider from "./CoursesSlider";
 import UpcomingSlider from "./UpcomingSlider";
 
-const Products: React.FC = () => {
-  const cources = [
-    {
-      id: 1,
-      title: "اسم الكورس",
-      trainer: "اسم المدرب",
-      icon: <CardIcon />,
-      soon: false,
-      price: 24,
-      courseDetails: {
-        vids: 52,
-        hours: 24,
-        mins: 45,
-      },
-    },
-    {
-      id: 2,
-      title: "اسم الكورس",
-      trainer: "اسم المدرب",
-      icon: <PythonIcon />,
-      soon: false,
-      price: 24,
-      courseDetails: {
-        vids: 52,
-        hours: 24,
-        mins: 45,
-      },
-    },
-    {
-      id: 3,
-      title: "اسم الكورس",
-      trainer: "اسم المدرب",
-      icon: <JavaIcon />,
-      soon: false,
-      price: 24,
-      courseDetails: {
-        vids: 52,
-        hours: 24,
-        mins: 45,
-      },
-    },
-    {
-      id: 4,
-      title: "اسم الكورس",
-      trainer: "اسم المدرب",
-      icon: <HTMLIcon />,
-      soon: false,
-      price: 24,
-      courseDetails: {
-        vids: 52,
-        hours: 24,
-        mins: 45,
-      },
-    },
-  ];
+export interface Trainer {
+  id: number;
+  first_name: string;
+  last_name: string;
+  leader: boolean;
+}
 
-  const upcoming = [
-    {
-      id: 1,
-      title: "اسم الكورس",
-      trainer: "اسم المدرب",
-      icon: <Diamond />,
-      soon: true,
-      courseDetails: {
-        vids: 52,
-        hours: 24,
-        mins: 45,
-      },
-    },
-    {
-      id: 2,
-      title: "اسم الكورس",
-      trainer: "اسم المدرب",
-      icon: <Bootstrap />,
-      soon: true,
-      courseDetails: {
-        vids: 52,
-        hours: 24,
-        mins: 45,
-      },
-    },
-    {
-      id: 3,
-      title: "اسم الكورس",
-      trainer: "اسم المدرب",
-      icon: <CSharpIcon />,
-      soon: true,
-      courseDetails: {
-        vids: 52,
-        hours: 24,
-        mins: 45,
-      },
-    },
-    {
-      id: 4,
-      title: "اسم الكورس",
-      trainer: "اسم المدرب",
-      icon: <PHPIcon />,
-      soon: true,
-      courseDetails: {
-        vids: 52,
-        hours: 24,
-        mins: 45,
-      },
-    },
-  ];
+export interface Course {
+  id: number;
+  title: string;
+  imageURL: string;
+  total_videos: number;
+  total_duration: string;
+  original_price: number;
+  currency: string;
+  status: "available" | "coming_soon";
+  trainers: Trainer[];
+}
+
+const Products: React.FC = () => {
+  const [courses, setCourses] = useState<Course[] | null>(null);
+
+  useEffect(() => {
+    fetch("https://sitev2.arabcodeacademy.com/wp-json/aca/v1/courses").then(
+      (res) => res.json().then((data) => setCourses(data.courses)),
+    );
+  }, []);
+
+  console.log(courses);
 
   return (
     <Box
@@ -136,31 +51,26 @@ const Products: React.FC = () => {
       }}
     >
       <CoursesSlider>
-        {cources.map((item) => (
-          <Box padding={"4"} key={item.id}>
-            <ProductCard
-              soon={false}
-              icon={item.icon}
-              title={item.title}
-              trainer={item.trainer}
-              price={item.price}
-              courseDetails={item.courseDetails}
-            />
-          </Box>
-        ))}
+        {!courses
+          ? null
+          : courses
+              .filter((item) => item.status === "available")
+              .map((item: Course) => (
+                <Box padding={"4"} key={item.id}>
+                  <ProductCard course={item} />
+                </Box>
+              ))}
       </CoursesSlider>
       <UpcomingSlider>
-        {upcoming.map((item) => (
-          <Box padding={"5"} key={item.id}>
-            <ProductCard
-              soon={true}
-              icon={item.icon}
-              title={item.title}
-              trainer={item.trainer}
-              courseDetails={item.courseDetails}
-            />
-          </Box>
-        ))}
+        {!courses
+          ? null
+          : courses
+              .filter((item) => item.status === "coming_soon")
+              .map((item: Course) => (
+                <Box padding={"5"} key={item.id}>
+                  <ProductCard course={item} />
+                </Box>
+              ))}
       </UpcomingSlider>
     </Box>
   );
